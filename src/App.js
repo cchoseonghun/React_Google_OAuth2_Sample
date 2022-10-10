fa3bcbee7f511eca58ff56947c9cbcafb88b0743
seonghun <cchoseonghun@gmail.com>
2022-10-10 23:22:54 +0900

    first upload

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react'
+import jwt_decode from 'jwt-decode'
 import './App.css';
 
 function App() {
+  const [user, setUser] = useState({});
+
+  function handleCallbackResponse(res){
+    console.log('Encoded JWT ID token: ' + res.credential);
+    let userObject = jwt_decode(res.credential);
+    console.log(userObject);
+    setUser(userObject);
+    document.querySelector('#G_OAuth_btn').hidden = true;
+  }
+
+  function handleSignOut(){
+    setUser({});
+    document.querySelector('#G_OAuth_btn').hidden = false;
+  }
+
+  useEffect(()=>{
+    /* global google */
+    google.accounts.id.initialize({
+      client_id: '클라이언트 ID', 
+      callback: handleCallbackResponse
+    })
+
+    google.accounts.id.renderButton(
+      document.getElementById('G_OAuth_btn'), 
+      { theme: 'outline', size: 'large' }
+    )
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id='G_OAuth_btn'></div>
+      { Object.keys(user).length != 0 &&
+        <button onClick={()=>handleSignOut()}>Sign Out</button>
+      }
+      { user &&
+        <div>
+          <img src={user.picture} />
+          <h3>{user.name}</h3>
+        </div>
+      }
     </div>
   );
 }
